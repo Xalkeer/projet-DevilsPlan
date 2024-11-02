0628980de87bd6a108239121812b4f62800a4bcb
Xalkeer <127451899+Xalkeer@users.noreply.github.com>
2024-11-02 16:34:33 +0100

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+    std::cout <<"le zizi oaioai" << std::endl;
+    std::cout <<"bite"<< std::endl;
+    std::
+}
+
+
